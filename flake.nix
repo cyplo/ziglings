82add8140e75ed2b6910f5d7c200ba83914779e1
Cyryl Płotnicki <cyplo@cyplo.net>
2022-01-02 11:32:21 +0000

    fix shell.nix compat

--- flake.nix
+++ flake.nix
@@ -1,17 +1,22 @@
 {
-  inputs = { 
-    nixpkgs = { 
+  inputs = {
+    nixpkgs = {
       type = "github";
       owner = "nixos";
       repo = "nixpkgs";
       ref = "e1eeb53e64159fbb2610ba7810ed511e4d5c10ca";
     };
-};
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-compat }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
+    in
+    {
       devShell.x86_64-linux =
         pkgs.mkShell { buildInputs = [ pkgs.zig ]; };
-   };
+    };
 }
